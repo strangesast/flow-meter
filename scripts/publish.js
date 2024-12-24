b6a817ba34c00f7956f67f2600ced1a4a71aa67a
Sam Zagrobelny <strangesast@gmail.com>
2024-12-24 16:45:22 -0500

    update test script

---
+++ scripts/publish.js
@@ -0,0 +1,49 @@
+const { setTimeout } = require('timers/promises');
+const mqtt = require('mqtt');
+const fs = require('fs');
+
+const KEY_PATH = '../driver/main/client.key';
+const CERT_PATH = '../driver/main/client.crt';
+const CA_PATH = '../driver/main/root.crt';
+const MQTT_BROKER_URI = process.env.MQTT_BROKER_URI;
+const TOPIC = 'flowmeter/device0/valve_status';
+
+const DEVICES = ['device0', 'device1'];
+
+const client = mqtt.connect({
+  host: MQTT_BROKER_URI,
+  port: 8883,
+  protocol: 'mqtts',
+  key: fs.readFileSync(KEY_PATH),
+  cert: fs.readFileSync(CERT_PATH),
+  ca: fs.readFileSync(CA_PATH),
+  rejectUnauthorized: true,
+});
+
+client.on('connect', async () => {
+  console.log('Connected.');
+
+  console.log(`Opening valves...`);
+  for (const device of DEVICES) {
+    await client.publishAsync(`flowmeter/${device}/valve_status`, "1");
+  }
+
+  console.log(`Getting some flow data...`);
+  await client.subscribeAsync(`flowmeter/+/flow`);
+  client.on('message', async (topic, message) => {
+    console.log(`Received message on topic "${topic}": ${message}`);
+  });
+  await setTimeout(10000);
+  await client.unsubscribeAsync(`flowmeter/+/flow`);
+
+  console.log(`Closing valves...`);
+  for (const device of DEVICES) {
+    await client.publishAsync(`flowmeter/${device}/valve_status`, "0");
+  }
+
+  client.end();
+});
+
+client.on('error', (error) => {
+  console.error('Connection error:', error);
+});
